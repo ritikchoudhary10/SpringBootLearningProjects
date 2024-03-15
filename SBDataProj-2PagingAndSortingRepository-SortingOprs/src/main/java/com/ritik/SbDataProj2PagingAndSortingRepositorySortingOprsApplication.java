52674a3006e8429fee38c82ce5ade35c1c31c08c
Ritik Choudhary <ritikmon@gmail.com>
2024-03-15 17:03:57 +0530

    Paging and sorting repository demo

---
+++ SBDataProj-2PagingAndSortingRepository-SortingOprs/src/main/java/com/ritik/SbDataProj2PagingAndSortingRepositorySortingOprsApplication.java
@@ -0,0 +1,13 @@
+package com.ritik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDataProj2PagingAndSortingRepositorySortingOprsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDataProj2PagingAndSortingRepositorySortingOprsApplication.class, args);
+	}
+
+}
