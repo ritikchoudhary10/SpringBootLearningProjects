12e842d1a634900c757a68279516f4393a2a7557
Ritik Choudhary <ritikmon@gmail.com>
2024-03-20 17:11:30 +0530

    RestControllerDemo project

---
+++ SBRestProj-1-RestControllerDemo/src/main/java/com/ritik/SbRestProj1RestControllerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ritik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRestProj1RestControllerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRestProj1RestControllerDemoApplication.class, args);
+	}
+
+}
