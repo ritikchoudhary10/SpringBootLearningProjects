6764ad5e87b1711431d42ee37fef0574b2c95d65
Ritik Choudhary <ritikmon@gmail.com>
2024-03-22 22:25:01 +0530

    ..

--- SBRestProj-8-ProviserApp-PathVariable/src/main/java/com/ritik/rest/PathVarDemo.java
+++ SBRestProj-8-ProviserApp-PathVariable/src/main/java/com/ritik/rest/PathVarDemo.java
@@ -11,10 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/provider-api")
 public class PathVarDemo {
 	
-	@GetMapping("/{data1}")
-	public ResponseEntity<String>  method1(@PathVariable("data1") Integer no) {
-		return new ResponseEntity<String>("data passed "+no , HttpStatus.OK);
-		
+	@GetMapping("/update/data1/data2")
+	public ResponseEntity<String>  method1(@PathVariable(name="data1",required=false) Integer no,
+																						@PathVariable(name="data1",required=false) Double name) {
+		return new ResponseEntity<String>("data passed " , HttpStatus.OK);
+	}
+	
+	@GetMapping("/update/{data1}/{data2}")
+	private ResponseEntity<String> method2(@PathVariable(name = "data1" ,required = false)     Integer first,
+																						@PathVariable(name = "data1" , required=false)	Double second) {
+		return new ResponseEntity<String>("method 2", HttpStatus.OK);
+	} 
+	
+	@GetMapping("/update/{data1}/data2")
+	private ResponseEntity<String> method3(@PathVariable(name = "data1" , required=false)     Integer first,
+																						@PathVariable(name = "data1",required=false)	Double second) {
+		return new ResponseEntity<String>("method3", HttpStatus.OK);
+	}
+	
+	@GetMapping("/update/data1/{data2}")
+	private ResponseEntity<String> method4(@PathVariable(name = "data1" ,required=false)     Integer first,
+																						@PathVariable(name = "data1",required=false)	    Double  second) {
+		return new ResponseEntity<String>("method 4", HttpStatus.OK);
 	}
 
 }
