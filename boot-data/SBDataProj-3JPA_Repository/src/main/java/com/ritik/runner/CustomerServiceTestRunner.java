b45ecb9fe5d091344762d38b8630474a4babb223
Ritik Choudhary <ritikmon@gmail.com>
2024-04-18 23:36:38 +0530

    ..

--- boot-data/SBDataProj-3JPA_Repository/src/main/java/com/ritik/runner/CustomerServiceTestRunner.java
+++ boot-data/SBDataProj-3JPA_Repository/src/main/java/com/ritik/runner/CustomerServiceTestRunner.java
@@ -1,5 +1,6 @@
 package com.ritik.runner;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +19,12 @@ public class CustomerServiceTestRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		/*System.out.println("====================================================================================================");
-		
-		//saving the data in database
-		Customer cust = new Customer("Ritik", "Indore", 0.0);
-		System.out.println(ser.saveCustomer(cust));
-		*/
+		/*	System.out.println("====================================================================================================");
+			
+			//saving the data in database
+			Customer cust = new Customer("Ritik", "Indore", 0.0);
+			System.out.println(ser.saveCustomer(cust));
+			*/
 		
 		/*System.out.println("====================================================================================================");
 		//Customer data to be fetched with example object
@@ -32,17 +33,24 @@ public class CustomerServiceTestRunner implements CommandLineRunner {
 		ser.showCustomersByExampleData(cust, false, "cName").forEach(System.out::println);
 		*/
 		
-		System.out.println("====================================================================================================");
+		/*System.out.println("====================================================================================================");
 		int id=2;
 		Optional<Customer> opt = ser.fetchCustomerById(id);
 		if (opt.isEmpty()) {
 			throw new IllegalArgumentException("Customer with id "+id+" doesn't exists..!");
 		}
-		System.out.println(opt.get());
+		System.out.println(opt.get());*/
 		
 		
+		/*System.out.println("====================================================================================================");
+		int id=2;
+		//System.out.println(ser.showCustomerById(id));
+		ser.showCustomerById(id);
+		*/
 		System.out.println("====================================================================================================");
-		System.out.println("====================================================================================================");
+		List<Integer> ids = List.of(8,9,10,7);
+		System.out.println(ser.deleteCustomersByIdsInBatch(ids));
+		
 		System.out.println("====================================================================================================");
 		
 	}
