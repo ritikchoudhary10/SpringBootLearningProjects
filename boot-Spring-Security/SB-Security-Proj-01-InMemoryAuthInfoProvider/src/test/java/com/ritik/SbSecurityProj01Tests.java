fa8a2e00d48341dfa66fae32c98dbd6322014cf6
Ritik Choudhary <ritikmon@gmail.com>
2024-05-17 17:30:12 +0530

    spring security TOC

--- boot-Spring-Security/SB-Security-Proj-01-InMemoryAuthInfoProvider/src/test/java/com/ritik/SbSecurityProj01Tests.java
+++ boot-Spring-Security/SB-Security-Proj-01-InMemoryAuthInfoProvider/src/test/java/com/ritik/SbSecurityProj01Tests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SbSecurityProj01InMemoryAuthInfoProviderFormBasedAuthenticationApplicationTests {
+class SbSecurityProj01Tests {
 
 	@Test
 	void contextLoads() {
